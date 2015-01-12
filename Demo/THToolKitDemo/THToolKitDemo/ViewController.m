4c17342c8d171138426f79dc3422da7c9a47fc79
viico_mac <viico_mac@viico-macdeMac-mini.local>
2015-01-12 17:53:16 +0800

    Initial commit

---
+++ Demo/THToolKitDemo/THToolKitDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  THToolKitDemo
+//
+//  Created by viico_mac on 15/1/12.
+//  Copyright (c) 2015年 HomTang. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "THToolKit.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
